18e4e5ccdff5da35636cc668295c34fdb230b824
Eva Fengel <45177675+evafengel@users.noreply.github.com>
2023-11-05 17:26:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+}
+
